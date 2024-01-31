110c0ec61bac7e86bf93027f7edf769e348c0f21
Shaik Sohail Ahamed <sohailqwerty7@gmail.com>
2024-01-31 15:43:16 +0530

    test commit

--- app/Navbar.jsx
+++ app/Navbar.jsx
@@ -4,14 +4,6 @@ import { Button } from '@/components/ui/button';
 export default function Navbar() {
   return (
     <main>
-      <div className="bg-transparent backdrop-blur-sm fixed top-2 w-96 rounded-lg h-12 ">
-        <ul className="flex justify-around items-center">
-          <li className="text-2xl font-semibold ">Logo</li>
-          <li className="text-2xl font-semibold ">Home</li>
-          <li className="text-2xl font-semibold ">About</li>
-          <li className="text-2xl font-semibold ">Contact</li>
-        </ul>
-      </div>
       <Link href="/articles/1">
         <Button className="mt-14">Article 1</Button>
       </Link>
